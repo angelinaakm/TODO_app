f76311cbce1cef0d0cd0c326e1c4a15931a00258
Angelina Kim <ang.km777@gmail.com>
2023-09-06 18:33:39 +0100

    new files for the app

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());       
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let taskList = []
+
+app.get("/addtask", function (req, res) {
+    res.render("index", {
+        tasklist: taskList,
+    });
+});
+
+
+app.post('/addtask', function (req, res) {
+    let newTask = req.body.newtask
+    taskList.push(newTask) 
+    res.redirect("/addtask");
+});
+
+app.listen(3000, function () {
+	console.log("Server started on port 3000.");
+});
