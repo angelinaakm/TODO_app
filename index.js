14609a30fb5a8305799f59471780b4fabfd2b31d
Angelina Kim <ang.km777@gmail.com>
2023-09-20 10:45:00 +0100

    adding style

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());       
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 let taskList = []
 
